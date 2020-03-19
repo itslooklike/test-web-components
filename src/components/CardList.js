987985d583fb2abd37d574010c79fcdf53275b9f
a.masalov <delsol@bk.ru>
2020-03-20 01:44:36 +0300

    import

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,9 +1,6 @@
-import { UserCard } from './UserCard.js'
-window.customElements.define('user-card', UserCard)
 const template = document.createElement('template')
 
 template.innerHTML = `
-  <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
   <div id="card-list"></div>
 `
 
@@ -15,23 +12,43 @@ export class CardList extends HTMLElement {
     this.container = this.shadowRoot.querySelector('#card-list')
   }
 
-  async connectedCallback() {
-    const response = await fetch('https://randomuser.me/api/?results=13')
-    const { results } = await response.json()
+  async initUserCard() {
+    const { UserCard } = await import('./UserCard.js')
+    window.customElements.define('user-card', UserCard)
+  }
 
-    const cards = results
-      .map(
-        ({ name: { first, last }, email, phone, picture: { large } }) => `
+  getCards(props) {
+    const {
+      name: { first, last },
+      email,
+      phone,
+      picture: { large },
+    } = props
+
+    return `
       <user-card name="${first} ${last}" avatar="${large}">
         <div slot="email">${email}</div>
         <div slot="phone">${phone}</div>
       </user-card>
-    `
-      )
-      .join('\n')
+    `.trim()
+  }
+
+  async connectedCallback() {
+    let data = 'Сервис не доступен'
+
+    try {
+      const response = await fetch('https://randomuser.me/api/?results=3')
+      const { results } = await response.json()
+
+      await this.initUserCard()
+
+      data = results.map(this.getCards).join('\n')
+    } catch (err) {
+      console.log(err)
+    }
 
     const node = document.createElement('div')
-    node.innerHTML = cards
+    node.innerHTML = data
     this.container.appendChild(node)
   }
 }
